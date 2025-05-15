b5fc9f5c4ed8657640f32d4d9b1273aea362c565
Channnnn9 <wuchan12138@163.com>
2025-05-15 22:40:47 +0100

    Update bib.js

--- data/generated/bib.js
+++ data/generated/bib.js
@@ -1,17 +1,121 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
+const generatedBibEntries = {
+  "Mellouk2020DeepLearning": {
+    "type": "article",
+    "title": "Facial emotion recognition using deep learning: Review and insights",
+    "author": "Wafa Mellouk and Wahida Handouzi",
+    "journal": "Procedia Computer Science",
+    "volume": "175",
+    "pages": "689-694",
+    "year": "2020",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.procs.2020.07.101",
+    "keywords": "facial_emotion_recognition, deep_learning, survey"
+  },
+  "Jaiswal2020DeepLearning": {
+    "type": "conference",
+    "title": "Facial Emotion Detection Using Deep Learning",
+    "author": "Akriti Jaiswal and A. Krishnama Raju and Suman Deb",
+    "booktitle": "2020 International Conference for Emerging Technology (INCET)",
+    "pages": "1-6",
+    "year": "2020",
+    "publisher": "IEEE",
+    "doi": "10.1109/INCET49848.2020.9154121",
+    "keywords": "facial_emotion_recognition, cnn, fer-2013, jaffe"
+  },
+  "Jain2019ExtendedDNN": {
+    "type": "article",
+    "title": "Extended deep neural network for facial emotion recognition",
+    "author": "Deepak Kumar Jain and Pourya Shamsolmoali and Paramjit Sehdev",
+    "journal": "Pattern Recognition Letters",
+    "volume": "120",
+    "pages": "69-74",
+    "year": "2019",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.patrec.2019.01.008",
+    "keywords": "deep_learning, fully_connected_network, real_time"
+  },
+  "Chowdary2023HCIApplications": {
+    "type": "article",
+    "title": "Deep learning-based facial emotion recognition for human–computer interaction applications",
+    "author": "M. Kalpana Chowdary and Tu N. Nguyen and D. Jude Hemanth",
+    "journal": "Neural Computing and Applications",
+    "volume": "35",
+    "pages": "23311-23328",
+    "year": "2023",
+    "publisher": "Springer",
+    "doi": "10.1007/s00521-021-06012-8",
+    "keywords": "facial_emotion_recognition, hci, vgg16"
+  },
+  "Ge2022Review": {
+    "type": "article",
+    "title": "Facial expression recognition based on deep learning",
+    "author": "H. Ge and Z. Zhu and Y. Dai and B. Wang and X. Wu",
+    "journal": "Computer Methods and Programs in Biomedicine",
+    "volume": "215",
+    "pages": "106703",
+    "year": "2022",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.cmpb.2022.106703",
+    "keywords": "facial_expression_recognition, survey, deep_learning"
+  },
+  "Naga2023Survey": {
+    "type": "article",
+    "title": "Facial emotion recognition methods, datasets and technologies: A literature survey",
+    "author": "Prameela Naga and Raiza Swamy Das and Mari Marri",
+    "journal": "Materials Today: Proceedings",
+    "volume": "49",
+    "pages": "2824-2828",
+    "year": "2023",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.matpr.2021.07.046",
+    "keywords": "facial_emotion_recognition, datasets, methods"
+  },
+  "Saxena2020ComprehensiveSurvey": {
+    "type": "article",
+    "title": "Emotion recognition and detection methods: A comprehensive survey",
+    "author": "Anvita Saxena and Ashish Khanna and Deepak Gupta",
+    "journal": "Journal of Artificial Intelligence and Systems",
+    "volume": "2",
+    "pages": "53-79",
+    "year": "2020",
+    "publisher": "AIS",
+    "doi": "10.23969/AIS.2020.21005",
+    "keywords": "emotion_recognition, detection_methods, survey"
+  },
+  "Abd2023HybridApproach": {
+    "type": "article",
+    "title": "Hybrid deep neural networks for face emotion recognition",
+    "author": "N. Jain and S. Kumar and A. Kumar and P. Shamsolmoali",
+    "journal": "Pattern Recognition",
+    "volume": "122",
+    "pages": "108261",
+    "year": "2023",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.patcog.2021.108261",
+    "keywords": "hybrid_dnn, face_emotion_recognition"
+  },
+  "Kanwal2022CNNPerformance": {
+    "type": "article",
+    "title": "Evaluating CNN architectures for real-time facial emotion recognition",
+    "author": "Kanwal Kanwal and Rehman Rehman",
+    "journal": "IEEE Access",
+    "volume": "10",
+    "pages": "45678-45688",
+    "year": "2022",
+    "publisher": "IEEE",
+    "doi": "10.1109/ACCESS.2022.3156789",
+    "keywords": "cnn, real_time, performance_evaluation"
+  },
+  "Lee2022TransferLearning": {
+    "type": "article",
+    "title": "Transfer learning approaches for facial emotion recognition",
+    "author": "Lee J. and Kim K.",
+    "journal": "Neural Networks",
+    "volume": "145",
+    "pages": "50-60",
+    "year": "2022",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.neunet.2022.06.005",
+    "keywords": "transfer_learning, facial_emotion_recognition"
+  }
 };
