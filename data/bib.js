d15748a4693c10ca7a80f260f4cad28cbcaaa446
Channnnn9 <wuchan12138@163.com>
2025-05-15 22:06:27 +0100

    Update bib.js

--- data/bib.js
+++ data/bib.js
@@ -1,94 +1,38 @@
 define({ "entries": [
-  @article{canal2022survey,
-  title={A survey on facial emotion recognition techniques: A state-of-the-art literature review},
-  author={Canal, Felipe Zago and others},
-  journal={Information Sciences},
-  volume={582},
-  pages={593--617},
-  year={2022},
-  publisher={Elsevier},
-  doi={10.1016/j.ins.2021.10.005}
-}
-@article{naga2023facial,
-  title={Facial emotion recognition methods, datasets and technologies: A literature survey},
-  author={Naga, Prameela and Marri, Swamy Das and Borreo, Raiza},
-  journal={Materials Today: Proceedings},
-  volume={49},
-  pages={2824--2828},
-  year={2023},
-  publisher={Elsevier},
-  doi={10.1016/j.matpr.2021.07.046}
-}
-@article{saxena2020emotion,
-  title={Emotion recognition and detection methods: A comprehensive survey},
-  author={Saxena, Anvita and Khanna, Ashish and Gupta, Deepak},
-  journal={Journal of Artificial Intelligence and Systems},
-  volume={2},
-  pages={53--79},
-  year={2020},
-  doi={10.33969/AIS.2020.21005}
-}
-@inproceedings{mellouk2020facial,
-  title={Facial emotion recognition using deep learning: Review and insights},
-  author={Mellouk, Wafa and Handouzi, Wahida},
-  booktitle={Procedia Computer Science},
-  volume={175},
-  pages={689--694},
-  year={2020},
-  publisher={Elsevier},
-  doi={10.1016/j.procs.2020.07.099}
-}
-@article{jaiswal2020facial,
-  title={Facial emotion detection using deep learning},
-  author={Jaiswal, Akriti and Raju, A Krishnama and Deb, Suman},
-  journal={International Journal of Engineering and Advanced Technology},
-  volume={9},
-  number={6},
-  pages={224--227},
-  year={2020},
-  doi={10.35940/ijeat.F9352.089620}
-}
-@article{jain2019extended,
-  title={Extended deep neural network for facial emotion recognition},
-  author={Jain, Prashant and Singh, Anurag and Singh, Rakesh Kumar},
-  journal={Pattern Recognition Letters},
-  volume={125},
-  pages={1--7},
-  year={2019},
-  publisher={Elsevier},
-  doi={10.1016/j.patrec.2019.01.008}
-}
-@article{chowdary2023deep,
-  title={Deep learning-based facial emotion recognition for human–computer interaction applications},
-  author={Chowdary, M Kalpana and Nguyen, Tu N and Hemanth, D Jude},
-  journal={Neural Computing and Applications},
-  volume={35},
-  pages={12345--12356},
-  year={2023},
-  publisher={Springer},
-  doi={10.1007/s00521-021-06012-8}
-}
-@article{zhang2024cross,
-  title={Cross-dataset generalization in deep learning},
-  author={Zhang, Xuyu and Huang, Haofan and Zhang, Dawei and Zhuang, Songlin and Han, Shensheng and Lai, Puxiang and Liu, Honglin},
-  journal={arXiv preprint arXiv:2410.11207},
-  year={2024}
-}
-@article{cantone2024machine,
-  title={Machine learning in network intrusion detection: A cross-dataset generalization study},
-  author={Cantone, Marco and Marrocco, Claudio and Bria, Alessandro},
-  journal={arXiv preprint arXiv:2402.10974},
-  year={2024}
-}
-@article{sarker2021machine,
-  title={Machine learning: Algorithms, real-world applications and research directions},
-  author={Sarker, I H},
-  journal={SN Computer Science},
-  volume={2},
-  number={3},
-  pages={160},
-  year={2021},
-  publisher={Springer},
-  doi={10.1007/s42979-021-00765-7}
-}    
+  {
+    "type": "article",
+    "key": "canal2022survey",
+    "title": "A survey on facial emotion recognition techniques: A state-of-the-art literature review",
+    "author": "Canal, Felipe Zago and others",
+    "journal": "Information Sciences",
+    "volume": "582",
+    "pages": "593-617",
+    "year": "2022",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.ins.2021.10.005"
+  },
+  {
+    "type": "article",
+    "key": "naga2023facial",
+    "title": "Facial emotion recognition methods, datasets and technologies: A literature survey",
+    "author": "Naga, Prameela and Marri, Swamy Das and Borreo, Raiza",
+    "journal": "Materials Today: Proceedings",
+    "volume": "49",
+    "pages": "2824-2828",
+    "year": "2023",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.matpr.2021.07.046"
+  },
+  {
+    "type": "article",
+    "key": "saxena2020emotion",
+    "title": "Emotion recognition and detection methods: A comprehensive survey",
+    "author": "Saxena, Anvita and Khanna, Ashish and Gupta, Deepak",
+    "journal": "Journal of Artificial Intelligence and Systems",
+    "volume": "2",
+    "pages": "53-79",
+    "year": "2020",
+    "doi": "10.33969/AIS.2020.21005"
+  }
 ]})
+
