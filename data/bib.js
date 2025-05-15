9014cdeb95bdf11d55667745086e643ad319202c
Channnnn9 <wuchan12138@163.com>
2025-05-15 22:20:06 +0100

    Update bib.js

--- data/bib.js
+++ data/bib.js
@@ -1,1 +1,38 @@
+define({ "entries": [
+  {
+    "type": "article",
+    "key": "canal2022survey",
+    "title": "A survey on facial emotion recognition techniques: A state-of-the-art literature review",
+    "author": "Canal, Felipe Zago and others",
+    "journal": "Information Sciences",
+    "volume": "582",
+    "pages": "593-617",
+    "year": "2022",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.ins.2021.10.005"
+  },
+  {
+    "type": "article",
+    "key": "naga2023facial",
+    "title": "Facial emotion recognition methods, datasets and technologies: A literature survey",
+    "author": "Naga, Prameela and Marri, Swamy Das and Borreo, Raiza",
+    "journal": "Materials Today: Proceedings",
+    "volume": "49",
+    "pages": "2824-2828",
+    "year": "2023",
+    "publisher": "Elsevier",
+    "doi": "10.1016/j.matpr.2021.07.046"
+  },
+  {
+    "type": "article",
+    "key": "saxena2020emotion",
+    "title": "Emotion recognition and detection methods: A comprehensive survey",
+    "author": "Saxena, Anvita and Khanna, Ashish and Gupta, Deepak",
+    "journal": "Journal of Artificial Intelligence and Systems",
+    "volume": "2",
+    "pages": "53-79",
+    "year": "2020",
+    "doi": "10.33969/AIS.2020.21005"
+  }
+]})
 
